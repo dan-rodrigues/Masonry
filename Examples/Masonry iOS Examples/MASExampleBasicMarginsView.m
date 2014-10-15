d8536e1b2df4644793eb38c541852eeeceda0e39
Dan Rodrigues <dan.rosa.rodrigues@gmail.com>
2014-10-15 22:27:06 +1100

    Added example view for margin constraints

--- Examples/Masonry iOS Examples/MASExampleBasicMarginsView.m
+++ Examples/Masonry iOS Examples/MASExampleBasicMarginsView.m
@@ -8,23 +8,77 @@
 
 #import "MASExampleBasicMarginsView.h"
 
+@interface MASExampleBasicMarginsView ()
+
+@property (nonatomic) UIButton *leftIncreaseButton;
+
+@property (nonatomic) UIView *containerView;
+
+@end
+
 @implementation MASExampleBasicMarginsView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (!self) return nil;
     
-    UIView *containerView = UIView.new;
-    containerView.backgroundColor = [UIColor grayColor];
-    [self addSubview:containerView];
+    self.containerView = UIView.new;
+    self.containerView.backgroundColor = [UIColor darkGrayColor];
+    //TODO: interactive updating of these margins. 4 buttons positioned around the center maybe
+    self.containerView.layoutMargins = UIEdgeInsetsMake(12, 24, 48, 96);
+    [self addSubview:self.containerView];
     
     UIView *childView = UIView.new;
-    childView.backgroundColor = [UIColor whiteColor];
-    [containerView addSubview:childView];
+    childView.backgroundColor = [UIColor lightGrayColor];
+    [self.containerView addSubview:childView];
     
-    //...
+    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.containerView.superview);
+    }];
+
+    //TODO: some shorthand way of saying .constraintedToMargins(?)
+    [childView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.containerView.mas_topMargin);
+        make.left.equalTo(self.containerView.mas_leftMargin);
+        make.bottom.equalTo(self.containerView.mas_bottomMargin);
+        make.right.equalTo(self.containerView.mas_rightMargin);
+    }];
+    
+    // setting the margin constraint directly seems wrong, but it's really setting the view's frame *relative* to the margin point (TODO: investigate)
+    
+    // adjustment buttons
+    UIButton *leftIncreaseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    leftIncreaseButton.backgroundColor = [UIColor whiteColor];
+    [leftIncreaseButton setTitle:@"+" forState:UIControlStateNormal];
+    [leftIncreaseButton addTarget:self action:@selector(adjustMargin:) forControlEvents:UIControlEventTouchUpInside];
+    [self.containerView addSubview:leftIncreaseButton];
+    
+    // TODO: remainder of buttons...
+    
+    [leftIncreaseButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        //-16 required to make it sit flush despite inset margin of 8
+        //this yeilds frame XY = 8, -4
+        //NOTE: this is tied to the top inset of 12 for containerView layoutMargins
+        //investigate how to cancel this effect
+        make.topMargin.equalTo(@8);
+        make.leftMargin.equalTo(@8);
+        
+//        make.left.equalTo(self.containerView).with.offset(10);
+//        make.centerY.equalTo(self.containerView);
+    }];
+
+    
+    leftIncreaseButton.layoutMargins = UIEdgeInsetsMake(8, 8, 8, 8);
+    
+    self.leftIncreaseButton = leftIncreaseButton;
     
     return self;
+}
+
+- (void)adjustMargin:(UIButton *)sender {
+    UIEdgeInsets insets = self.containerView.layoutMargins;
+    insets.left += 10.0;
+    self.containerView.layoutMargins = insets;
 }
 
 @end
